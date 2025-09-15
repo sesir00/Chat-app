563c3b9cc856b9c37a2b165df4cf198efbe2f282
Shishir Chapagain <110799052+sesir00@users.noreply.github.com>
2025-09-15 20:00:26 +0545

    added clerk auth in client

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+
+function App() {
+  return (
+    <>
+      <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
+  )
+}
+
+export default App
